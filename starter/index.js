fddcd2dce83db9b2ade673dfd4388fcaf49dadd0
ZuzanaElt <zuzanaeltom@gmail.com>
2023-03-05 19:05:14 +0000

    addded loop to include all team members

--- starter/index.js
+++ starter/index.js
@@ -104,7 +104,50 @@ const internData = [
     },
 ]
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+function nextEmployee (){
 
+            
+    inquirer
+        .prompt([
+            {
+                type: 'rawlist',
+                name: 'role',
+                message: "choose next employee's role",
+                choices: ['engineer', 'intern','finish building the team'],
+            },
+        ])
+        .then(roleChoice => {
+            console.log('chosen next role is: ',roleChoice)
+            if (roleChoice.role ==='engineer'){
+                inquirer
+                    .prompt(engineerData)
+                    .then(engineerAns =>{
+                        let engineer = new Engineer(engineerAns.name, engineerAns.id, engineerAns.email, engineerAns.github)
+                        console.log ('engineer: ', engineer)
+                        team.push(engineer)
+                        console.log('team with manager and engineer: ', team)
+                        nextEmployee()
+                    })
+            }else if (roleChoice.role ==='intern'){
+                inquirer
+                    .prompt(internData)
+                    .then(internAns =>{
+                        let intern = new Intern (internAns.name, internAns.id, internAns.email, internAns.school)
+                        console.log ('intern: ', intern)
+                        team.push(intern)
+                        console.log('team with manager and intern: ', team)
+                        nextEmployee()
+                        //need to add to array of team
+                    })
+            } else {
+                console.log ('team without addition',team)
+                console.log('html needs to be generated here')
+                return
+            }
+
+
+        })
+}
 
 inquirer
     .prompt( managerQ)
@@ -113,46 +156,14 @@ inquirer
 
             let manager = new Manager(answers.name, answers.id, answers.email,answers.officeNumber);
             console.log('manager is: ', manager);
-            //const managerRole = managerNew.getRole()//this works const testing = managerNew.getRole()
-            //manager.push(managerRole)//adding role to manager
 
-            //need to add to array of team
-        
-            inquirer
-                .prompt([
-                    {
-                        type: 'rawlist',
-                        name: 'role',
-                        message: "choose next employee's role",
-                        choices: ['engineer', 'intern','finish building the team'],
-                    },
-                ])
-                .then(roleChoice => {
-                    console.log('chosen next role is: ',roleChoice)
-                    if (roleChoice.role ==='engineer'){
-                        inquirer
-                             .prompt(engineerData)
-                            .then(engineerAns =>{
-                                let engineer = new Engineer(engineerAns.name, engineerAns.id, engineerAns.email, engineerAns.github)
-                                console.log ('engineer: ', engineer)
-                                //need to add to array of team
-                            })
-                    }else if (roleChoice.role ==='intern'){
-                        inquirer
-                             .prompt(internData)
-                            .then(internAns =>{
-                                let intern = new Intern (internAns.name, internAns.id, internAns.email, internAns.school)
-                                console.log ('intern: ', intern)
-                                //need to add to array of team
-                            })
-                    } else {
-                        console.log('html needs to be generated here')
-                    }
-
-
-                })
+            //
+            team.push(manager)
+            console.log('team is with manager: ', team)
+            nextEmployee();
             
-            })
+        })
+        
 
 
 
